cf7430907b9f32c04ac615d8563b0487f74bda11
Alexander Golovanov <a-golovanov@users.noreply.github.com>
2018-06-25 20:46:39 -0500

    WIP: change default maxHeightRatio

--- example/app/sheets/simple-choices.js
+++ example/app/sheets/simple-choices.js
@@ -2,6 +2,7 @@ import casual from "casual-browserify";
 
 export default () => (
     {
+        maxHeightRatio: 0.7,
         title: casual.title,
         message: casual.description,
         choices: casual.array_of_words( casual.integer( 0, 7 ) ).map( choice => ( { label: choice, value: choice } ) )
